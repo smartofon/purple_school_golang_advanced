44a8e00b9bf6cb044ee34008c907d83dc2f7bc52
luravele <kova76@mail.ru>
2025-09-06 08:57:51 +0500

    --

--- 4_3_speeding_up_work/main.go
+++ 4_3_speeding_up_work/main.go
@@ -2,15 +2,48 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 )
 
 func main() {
 
-	for i := 0; i < 10; i++ {
-		go fmt.Println("gourutine", i)
+	n := 10
+
+	fmt.Printf("Выполняем %d соединений\n", n)
+
+	fmt.Println("Без горутин")
+
+	t := time.Now()
+
+	for i := 0; i < n; i++ {
+		getHttpCode(i)
 	}
 
-	time.Sleep(time.Second * 5)
+	fmt.Printf("Время исполнения без горутин: %v\n", time.Since(t))
 
+	fmt.Println("С горутинами")
+
+	t = time.Now()
+
+	for i := 0; i < n; i++ {
+		go getHttpCode(i)
+	}
+
+	// горутины могут отработать быстрее,
+	// но пока ограничим исполнения в разы по отношению к предыдущему участку
+	// с использованием задержки исполнения
+	time.Sleep(time.Second)
+
+	fmt.Printf("Время исполнения sleep: %v\n", time.Since(t))
+
+}
+
+func getHttpCode(n int) {
+	resp, err := http.Get("https://google.com")
+	if err == nil {
+		fmt.Printf("Код ответа %d: %d\n", n, resp.StatusCode)
+	} else {
+		fmt.Printf("Ошибка: %s", err.Error())
+	}
 }
